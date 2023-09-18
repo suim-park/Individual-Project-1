bec1a71b2d369758da6616a2b62fd74fd368a7c7
suim.park <suim.park@duke.edu>
2023-09-18 17:48:17 -0400

    Edit lib.py file

--- lib.py
+++ lib.py
@@ -1,1 +1,11 @@
 # Build lib.py to adjust any system
+import pandas as pd
+
+
+# Load the dataset, which is .csv.
+def load_data(dataset):
+    """
+    Load data from a file and return a DataFrame.
+    """
+    data = pd.read_csv(dataset)
+    return data
